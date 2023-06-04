2d97f51da7d82a1d47e7a2a40b933e38a26e96ff
Parth Padhiyar <parthpadhiyar31@gmail.com>
2023-06-04 17:29:59 -0400

    Updating 'Developer' to 'developer' to keep codes consistent

--- Week-4/modules/game.js
+++ Week-4/modules/game.js
@@ -3,7 +3,7 @@ let mongoose = require('mongoose');
 //create a model class
 let gameModel = mongoose.Schema({
     name: String,
-    Developer: String,
+    developer: String,
     released: String,
     description: String,
     price: Number
