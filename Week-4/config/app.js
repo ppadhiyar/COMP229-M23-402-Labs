8a0bc662851ab61ff7423d3aa280b10c5589ec12
Parth Padhiyar <parthpadhiyar31@gmail.com>
2023-06-04 21:07:06 -0400

    Adding title for error.ejs

--- Week-4/config/app.js
+++ Week-4/config/app.js
@@ -51,7 +51,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error', { title: 'Error'});
 });
 
 module.exports = app;
